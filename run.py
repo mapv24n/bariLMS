2e231664da8fa057124ba5c0b7222642a4348c41
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2026-03-20 09:52:54 -0500

    feat: Add dotenv support to load environment variables from `.env` file.

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import sys
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "internal"))
 
 from bari_lms import create_app
